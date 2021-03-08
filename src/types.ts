e92af27d2b230cdd68c14c1944c22162cfec527f
JonasKruckenberg <iterpre@protonmail.com>
2021-03-08 17:08:52 +0100

    docs: add comments explaining types

--- src/types.ts
+++ src/types.ts
@@ -31,17 +31,35 @@ export interface PluginOptions {
      */
     customOutputFormats: OutputFormat[]
 }
+
+/**
+ * A Directive is a function that takes in the parsed parameters and returns a function that applies some transformation to the image.
+ * You can also return null to indicate that you don't want to handle this specific image (i.e. this directive is not applicable).
+ */
+export type Directive<A = {}> = (cfg: DirectiveOptions & A, ctx: DirectiveContext) => ImageTransformation | null
+
+/**
+ * A function that takes an image applies some transformations and returns the image.
+ */
 export type ImageTransformation = (image: Sharp) => Sharp
 
-export type DirectiveOptions = Record<string,any>
 export type OutputFormat = (src:URL, outputMetadatas: Record<string, any>[]) => any
+
+export type DirectiveOptions = Record<string, any>
 
 
 export interface DirectiveContext {
+    /**
+     * Use this function to indicate that you used some directive.
+     * This is used to warn the user about unused directives.
+     */
     useParam: (param: string) => void
+    /**
+     * Add a key value pair to the output metadata.
+     * The resulting metadata object will be available when importing an image with the `?metadata` directive
+     */
     setMetadata: (key: string, value: any) => void
 }
 
-export type Directive<A = {}> = (cfg: DirectiveOptions & A, ctx: DirectiveContext) => ImageTransformation
 
 export type ImageFormat = 'heic' | 'heif' | 'avif' | 'jpeg' | 'jpg' | 'png' | 'tiff' | 'webp' | 'gif'
