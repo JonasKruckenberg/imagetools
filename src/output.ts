d375b50a57c5755d3ec77f233dafde7c41211247
JonasKruckenberg <iterpre@protonmail.com>
2021-03-08 20:18:42 +0100

    refactor: rename from output to format

--- src/output.ts
+++ src/output.ts
@@ -1,18 +1,18 @@
 import { OutputFormat } from "./types"
 
-export const metadataOutput: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
+export const metadataFormat: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
     if (!src.searchParams.has('metadata') && !src.searchParams.has('meta')) return null
 
     return outputMetadatas.length === 1 ? outputMetadatas[0] : outputMetadatas
 }
 
-export const srcsetOutput: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
+export const srcsetFormat: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
     if (!src.searchParams.has('srcset')) return null
 
     return outputMetadatas.length === 1 ? outputMetadatas[0] : outputMetadatas
 }
 
-export const urlOutput: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
+export const urlFormat: OutputFormat = (src: URL, outputMetadatas: Record<string, any>[]) => {
     const out: string[] = outputMetadatas.map(metadata => metadata.src)
 
     return out.length == 1 ? out[0] : out
