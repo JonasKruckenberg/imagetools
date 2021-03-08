98525a46eb48e658ae4c3b1e4d4ac246e9edbb1b
JonasKruckenberg <iterpre@protonmail.com>
2021-03-08 15:54:22 +0100

    fix: export builtin directives

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,27 +1,3 @@
-import { sizeDirective } from './size'
-import { fitDirective } from './fit'
-import { widthDirective } from './width'
-import { heightDirective } from './height'
-import { formatDirective } from './format'
-import { positionDirective } from './position'
-import { kernelDirective } from './kernel'
-
-export interface Directive {
-    name: string,
-    with?: string[],
-    without?: string[],
-    test: (key: string, value: string) => boolean
-    transform?: (key: string, value: string) => Record<string, any>
-}
-
-export function directives(): Directive[] {
-    return [
-        sizeDirective,
-        widthDirective,
-        heightDirective,
-        formatDirective,
-        positionDirective,
-        fitDirective,
-        kernelDirective
-    ]
-}
+export * from './resize'
+export * from './format'
+export * from './rotate'
