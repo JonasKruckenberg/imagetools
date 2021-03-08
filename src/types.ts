e3ffc2f321add8d2b64906af48ef8794b727b728
JonasKruckenberg <iterpre@protonmail.com>
2021-03-08 15:55:07 +0100

    feat: add generic types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+import type { Sharp } from 'sharp'
+
+export type ImageTransformation = (image: Sharp) => Sharp
+
+export type DirectiveOptions = Record<string,any>
+
+export interface DirectiveContext {
+    useParam: (param: string) => void
+    setMetadata: (key: string, value: any) => void
+}
+
+export type Directive<A = {}> = (cfg: DirectiveOptions & A, ctx: DirectiveContext) => ImageTransformation
