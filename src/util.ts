e02d7e3f2c3f33cfc9847910e9f9266966bb7a12
JonasKruckenberg <iterpre@protonmail.com>
2021-03-08 22:27:24 +0100

    feat: split on semicolons (;) instead of commas (,) to avoid srcset parser confusion

--- src/util.ts
+++ src/util.ts
@@ -6,7 +6,7 @@ export const cartesian = (...a: any[]) => a.reduce((a, b) => a.flatMap(d => b.ma
 
 export function buildDirectiveOptions(src: URL): DirectiveOptions[] {
     return Array.from(src.searchParams.entries())
-        .map<[string, string[]]>(([key, value]) => [key, value.split(',')])
+        .map<[string, string[]]>(([key, value]) => [key, value.split(';')])
         .map(([key, values]) => values.map(v => ({ [key]: v })))
         .reduce((prev, cur) => prev.length ? cartesian(prev, cur) : cur, [])
         .map((options: Record<string, any>[]) => Array.isArray(options) ? Object.assign({}, ...options) : options)
