23f1e3fdd4169b234941ee40dc010cb6deb5f9f5
JonasKruckenberg <iterpre@protonmail.com>
2021-03-09 12:18:02 +0100

    docs: add srcset example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png?webp&nonexistent" />
+  <picture>
+    <source srcset="./assets/logo.png?width=300;400;500&avif&srcset" type="image/avif" />
+    <source srcset="./assets/logo.png?width=300;400;500&webp&srcset" type="image/webp" />
+    <img alt="Vue logo" src="./assets/logo.png?webp" />
+  </picture>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
