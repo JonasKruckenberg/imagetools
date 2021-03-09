059318d710b834ffb8355a8760a632af7bdf93f1
JonasKruckenberg <iterpre@protonmail.com>
2021-03-09 23:58:39 +0100

    feat: add the tint directive

---
+++ src/directives/tint.ts
@@ -0,0 +1,25 @@
+import { Directive } from "../types";
+
+interface TintOptions {
+    tint: string
+}
+
+/**
+ * Tints the image using the provided chroma while preserving the image luminance. 
+ * If the image has han alpha channel it will be untouched.
+ * 
+ * @name Tint
+ * @category "Import Directives"
+ * @keyword `tint`
+ * @type _string_
+ */
+export const tint: Directive<TintOptions> = ({ tint:color }, { useParam, setMetadata }) => {
+    if(!color) return null
+
+    useParam('tint')
+    setMetadata('tint',color)
+
+    return function tintTransform(image) {
+        return image.tint(color)
+    }
+}
