a2bc249f96ca45b627640f8fa3ec81765f314ceb
JonasKruckenberg <iterpre@protonmail.com>
2021-03-09 12:17:28 +0100

    fix: parse blur argument correctly
    using the blur function in boolean mode was not taken into consideration.

--- src/directives/blur.ts
+++ src/directives/blur.ts
@@ -5,9 +5,9 @@ interface FlipOptions {
 }
 
 export const blur: Directive<FlipOptions> = ({ blur: _blur }: DirectiveOptions, ctx: DirectiveContext) => {
-    const blur = parseFloat(_blur)
+    const blur = parseFloat(_blur) || _blur === '' || _blur === 'true'
 
-    if(isNaN(blur) || blur < 0.3 || blur > 1000) return null
+    if(!blur || typeof blur === 'number' && (blur < 0.3 || blur > 1000)) return null
 
     ctx.useParam('blur')
     ctx.setMetadata('blur', blur)
