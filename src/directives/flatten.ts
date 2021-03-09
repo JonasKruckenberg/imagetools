4df4c291fbc9a71d0503bcc1b6ea3298b277c176
JonasKruckenberg <iterpre@protonmail.com>
2021-03-09 23:58:11 +0100

    feat: add the flatten directive

---
+++ src/directives/flatten.ts
@@ -0,0 +1,28 @@
+import { Directive } from "../types";
+import { background as getBackground } from './background'
+
+interface FlattenOptions {
+    flatten: ''
+    background: string
+}
+
+/**
+ * This directive will remove the aplha channel of the image, reducing filesize.
+ * Transparent pixels will be merged with the color set by the [background directive](#background).
+ * 
+ * @name Flatten
+ * @category "Import Directives"
+ * @keyword `flatten`
+ * @type _boolean_
+ */
+export const flatten: Directive<FlattenOptions> = (opts, ctx) => {
+    const background = getBackground(opts,ctx) || undefined
+
+    if (opts.flatten !== '') return null
+
+    return function flattenTransform(image) {
+        return image.flatten({
+            background
+        })
+    }
+}
