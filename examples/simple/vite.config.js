9fbd26af47f36772ef8f2aa6a8a014e71596732e
JonasKruckenberg <iterpre@protonmail.com>
2021-03-10 15:45:15 +0100

    docs: add simple example

--- examples/simple/vite.config.js
+++ examples/simple/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { imagetools } from '../dist'
+import { imagetools } from '../../dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     imagetools({
-      cache: false
+      force: true
     })
   ]
 })
