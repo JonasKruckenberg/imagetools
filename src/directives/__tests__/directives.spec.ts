b4de882616d1ab5d23396411f5bd0b30df51362d
JonasKruckenberg <iterpre@protonmail.com>
2021-03-10 12:02:08 +0100

    test: add tests to ensure directives are correctly exported

---
+++ src/directives/__tests__/directives.spec.ts
@@ -0,0 +1,55 @@
+import * as directives from '../'
+
+describe('directives',() => {
+    it('exports "background"',() => {
+        expect('background' in directives).toBeTruthy()
+    })
+    it('exports "blur"',() => {
+        expect('blur' in directives).toBeTruthy()
+    })
+    it('exports "fit"',() => {
+        expect('fit' in directives).toBeTruthy()
+    })
+    it('exports "flip"',() => {
+        expect('flip' in directives).toBeTruthy()
+    })
+    it('exports "flop"',() => {
+        expect('flop' in directives).toBeTruthy()
+    })
+    it('exports "flatten"',() => {
+        expect('flatten' in directives).toBeTruthy()
+    })
+    it('exports "format"',() => {
+        expect('format' in directives).toBeTruthy()
+    })
+    it('exports "grayscale"',() => {
+        expect('grayscale' in directives).toBeTruthy()
+    })
+    it('exports "invert"',() => {
+        expect('invert' in directives).toBeTruthy()
+    })
+    it('exports "kernel"',() => {
+        expect('kernel' in directives).toBeTruthy()
+    })
+    it('exports "median"',() => {
+        expect('median' in directives).toBeTruthy()
+    })
+    it('exports "normalize"',() => {
+        expect('normalize' in directives).toBeTruthy()
+    })
+    it('exports "position"',() => {
+        expect('position' in directives).toBeTruthy()
+    })
+    it('exports "quality"',() => {
+        expect('quality' in directives).toBeTruthy()
+    })
+    it('exports "resize"',() => {
+        expect('resize' in directives).toBeTruthy()
+    })
+    it('exports "rotate"',() => {
+        expect('rotate' in directives).toBeTruthy()
+    })
+    it('exports "tint"',() => {
+        expect('tint' in directives).toBeTruthy()
+    })
+})
