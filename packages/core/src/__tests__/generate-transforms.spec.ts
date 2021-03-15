9c05132a5875a4dc9fc41ea955b40a3c6df2f562
JonasKruckenberg <iterpre@protonmail.com>
2021-03-15 15:17:41 +0100

    test: reflect generateTransforms signature change

--- packages/core/src/__tests__/generate-transforms.spec.ts
+++ packages/core/src/__tests__/generate-transforms.spec.ts
@@ -1,10 +1,12 @@
 import { Directive } from '../types'
-import { generateTransforms } from '../generate-transforms'
+import { generateTransforms } from '../index'
 
 describe('applyTransforms', () => {
     it('returns the transformations array', () => {
         const options = { width: 300, height: 100 }
-        const dirs = [() => i => i] as Directive[] // a mock directive
+        const dirs:Record<string,Directive> = {
+            mock: () => i => i // a mock directive
+        }
 
         const { transforms } = generateTransforms(options, dirs)
 
@@ -14,12 +16,12 @@ describe('applyTransforms', () => {
 
     it('returns the collected metadata', () => {
         const options = {}
-        const dirs: Directive[] = [
-            (_, ctx) => {
+        const dirs:Record<string,Directive> = {
+            mock: (_, ctx) => {
                 ctx.addMetadata('foo', 'bar')
                 return (img) => img
             }
-        ]
+        }
 
         const { metadata } = generateTransforms(options, dirs)
 
@@ -28,12 +30,12 @@ describe('applyTransforms', () => {
 
     it('returns the recognized parameters', () => {
         const options = {}
-        const dirs = [
-            (_, ctx) => {
+        const dirs:Record<string,Directive> = {
+            mock: (_, ctx) => {
                 ctx.useParam('foo')
                 return (img) => img
             }
-        ] as Directive[]
+        }
 
         const { parametersUsed } = generateTransforms(options, dirs)
 
@@ -43,7 +45,11 @@ describe('applyTransforms', () => {
     it('filters out directives that return undefined', () => {
         {
             const options = { width: 300, height: 100 }
-            const dirs = [() => undefined, () => undefined, () => undefined] as Directive[]
+            const dirs:Record<string,Directive> = {
+                'm1': () => undefined,
+                'm2': () => undefined,
+                'm3': () => undefined
+            }
 
             const { transforms } = generateTransforms(options, dirs)
 
@@ -52,7 +58,11 @@ describe('applyTransforms', () => {
         }
         {
             const options = { width: 300, height: 100 }
-            const dirs = [() => () => { }, () => () => { }, () => undefined] as Directive[]
+            const dirs:Record<string,Directive> = {
+                'm1': () => i => i,
+                'm2': () => i => i,
+                'm3': () => undefined
+            }
 
             const { transforms } = generateTransforms(options, dirs)
 
