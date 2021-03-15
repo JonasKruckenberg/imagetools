70cd9cd54ec227f9fe805cfff2b287baaba413bf
JonasKruckenberg <iterpre@protonmail.com>
2021-03-15 15:20:37 +0100

    feat: add output formats

---
+++ packages/vite/src/output-formats.ts
@@ -0,0 +1,31 @@
+import type { ImageConfig } from 'imagetools-core'
+import { OutputFormat } from './types'
+
+export const urlFormat: OutputFormat = (metadatas) => {
+    const urls: string[] = metadatas.map(metadata => metadata.src)
+
+    return urls.length == 1 ? urls[0] : urls
+}
+
+export const srcsetFormat: OutputFormat = (metadatas: ImageConfig[]) => {
+    const sources = metadatas.reduce((prev, meta) => {
+        if (prev) {
+            return `${prev}, ${meta.src} ${meta.width}w`
+        } else {
+            return `${meta.src} ${meta.width}w`
+        }
+    }, '')
+
+    return sources
+}
+
+export const metadataFormat: OutputFormat = (metadatas: ImageConfig[]) => {
+    return metadatas.length === 1 ? metadatas[0] : metadatas
+}
+
+export const builtinOutputFormats = {
+    url: urlFormat,
+    srcset: srcsetFormat,
+    metadata: metadataFormat,
+    meta: metadataFormat
+}
