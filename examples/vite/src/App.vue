852bdfc36ab519809f21bd7c3041c4b6075a7f5d
JonasKruckenberg <iterpre@protonmail.com>
2021-03-15 15:22:59 +0100

    docs: add basic example

---
+++ examples/vite/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" :src="Logo.src" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Logo from './assets/logo.png?meta&foo=bar'
+
+console.log(Logo);
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return { Logo }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ examples/vite/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import imagetools from'../../packages/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    imagetools()
+  ]
+})
