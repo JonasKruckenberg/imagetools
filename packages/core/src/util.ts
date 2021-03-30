e09434c65548230bfdce964264c0f9cc83aadd86
JonasKruckenberg <iterpre@protonmail.com>
2021-03-30 19:45:37 +0200

    fix: cache key generation

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -2,16 +2,14 @@ import sharp from "sharp";
 import { ImageConfig } from "./types";
 import { createHash } from 'crypto'
 
-export function loadImageFromDisk(path: string) {
+export function loadImage(path: string) {
     return sharp(path)
 }
 
-export function loadImageFromBuffer(buf: Buffer) {
-    return sharp(buf)
-}
-
 export function generateImageID(url:URL, config:ImageConfig) {
-    const baseURL = new URL(url.origin + url.pathname)
+    const baseURL = url.host 
+        ? new URL(url.origin + url.pathname)
+        : new URL(url.protocol + url.pathname)
     
     return createHash('sha1').update(baseURL.href).update(JSON.stringify(config)).digest('hex')
 }
