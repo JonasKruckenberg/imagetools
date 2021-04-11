7a97f520dd123ced9fc08ec99ff6f8b348d35072
JonasKruckenberg <iterpre@protonmail.com>
2021-04-11 13:27:25 +0200

    chore: fix jest module resolution

--- packages/rollup/jest.config.js
+++ packages/rollup/jest.config.js
@@ -1,11 +1,24 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
+
+/**
+ * This allows us to 
+ */
+const paths = {
+  'imagetools-core': ['../core/src']
+}
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: [ "**/?(*.)+(spec|test).[jt]s?(x)" ],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        paths
+      },
+    },
+  },
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>/' }),
   collectCoverageFrom: [
     "src/**/*.ts",
     "!**/__tests__/**"
--- packages/vite/jest.config.js
+++ packages/vite/jest.config.js
@@ -1,11 +1,21 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
+
+const paths = {
+  'imagetools-core': ['../core/src']
+}
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: [ "**/?(*.)+(spec|test).[jt]s?(x)" ],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        paths
+      },
+    },
+  },
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>/' }),
   collectCoverageFrom: [
     "src/**/*.ts",
     "!**/__tests__/**"
