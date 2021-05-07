23d8ab35d516f7d96b77932a0d245feee725a254
JonasKruckenberg <iterpre@protonmail.com>
2021-05-07 17:24:51 +0200

    test: update parse-url tests to reflect renamed fns

--- packages/core/src/__tests__/parse-url.spec.ts
+++ packages/core/src/__tests__/parse-url.spec.ts
@@ -1,10 +1,26 @@
-import { parseURL } from '../lib/parse-url'
+import { parseURL, extractEntries } from '../lib/parse-url'
 
 describe('parseURL', () => {
+    it('returns an URL object', () => {
+        const res = parseURL('/test/example.jpg?foo=bar')
+
+        expect(res).toBeInstanceOf(URL)
+    })
+
+    it('escapes the # symbol', () => {
+        const res = parseURL('/test/example.jpg?bg=#fff&foo=bar')
+
+        expect(res).toBeInstanceOf(URL)
+        expect(res.searchParams.get('foo')).toEqual('bar')
+        expect(res.searchParams.get('bg')).toEqual('#fff')
+    })
+})
+
+describe('extractEntries', () => {
     it('returns an array of entries', () => {
         const src = new URL('/example.jpg?foo=bar', 'file://')
 
-        const entries = parseURL(src)
+        const entries = extractEntries(src)
 
         expect(entries).toBeInstanceOf(Array)
     })
@@ -12,7 +28,7 @@ describe('parseURL', () => {
     it('returns a valid array of entries', () => {
         const src = new URL('/example.jpg?foo=bar&hello=world&width=300', 'file://')
 
-        const entries = parseURL(src)
+        const entries = extractEntries(src)
         // this will throw and fail the test if entries is not a valid array of entries
         const asObject = Object.fromEntries(entries)
 
@@ -24,7 +40,7 @@ describe('parseURL', () => {
     it('splits the arguments at the ";" char', () => {
         const src = new URL('/test.jpg?width=300;400;500', 'file:///')
 
-        const entries = parseURL(src)
+        const entries = extractEntries(src)
         const asObject = Object.fromEntries(entries)
 
         expect(asObject).toHaveProperty('width', ['300', '400', '500'])
