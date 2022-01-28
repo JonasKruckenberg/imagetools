1ba1b0e3291d330c158c4915a0c247cd1fbb5ef5
Jonas Kruckenberg <iterpre@protonmail.com>
2022-01-29 00:57:51 +0100

    Switch to Pnpm (#315)
    * switch to pnpm
    
    * switch to `@rollup/plugin-typescript`
    
    * update workflow files
    
    * fix test workflow
    
    * Update test.yml
    
    * Update coverage.yml

--- packages/core/src/__tests__/apply-transforms.spec.ts
+++ packages/core/src/__tests__/apply-transforms.spec.ts
@@ -5,7 +5,7 @@ import { join } from 'path'
 describe('applyTransforms', () => {
   let img: Sharp
   beforeEach(() => {
-    img = sharp(join(__dirname, './__fixtures__/pexels-allec-gomes-5195763.png'))
+    img = sharp(join(__dirname, './__fixtures__/with-metadata.png'))
   })
 
   it('applies the transforms to the image', async () => {
@@ -16,11 +16,35 @@ describe('applyTransforms', () => {
     expect(t).toBeCalled()
   })
 
+  it('strips metadata by default', async () => {
+    const t = jest.fn((i) => i)
+
+    const { metadata } = await applyTransforms([t], img, true)
+
+    expect(t).toBeCalled()
+    expect(metadata).not.toHaveProperty('icc')
+    expect(metadata).not.toHaveProperty('xmp')
+    expect(metadata).not.toHaveProperty('exif')
+    expect(metadata).not.toHaveProperty('tifftagPhotoshop')
+    expect(metadata).not.toHaveProperty('iptc')
+  })
+
+  it('metadata stripping can be disabled', async () => {
+    const t = jest.fn((i) => i)
+
+    const { metadata } = await applyTransforms([t], img, false)
+
+    expect(t).toBeCalled()
+    expect(metadata).toHaveProperty('icc')
+    expect(metadata).toHaveProperty('xmp')
+  })
+
   it('returns the image data & info', async () => {
     const t = jest.fn((i) => i)
 
     const res = await applyTransforms([t], img)
 
+    expect(t).toBeCalled()
     expect(res).toHaveProperty('image')
     expect(res).toHaveProperty('metadata')
   })
