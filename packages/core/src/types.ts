0c4471cd6ed8deb26f2f739886c26f05dfcdd3c7
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-24 21:45:00 -0700

    feat: add picture and source output formats

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -21,4 +21,25 @@ export interface TransformResult {
   metadata: Record<string, unknown>
 }
 
+/**
+ * The JS object returned by the image import.
+ */
 export type OutputFormat = (args?: string[]) => (metadata: ImageConfig[]) => unknown
+
+/**
+ * The source output format.
+ * Can be used to dynamically construct a sourceset string when you need to
+ * choose between width descriptor, pixel density descriptor, or no descriptor.
+ */
+export interface Source {
+  src: string;
+  w: number;
+}
+
+/**
+ * The picture output format.
+ */
+export interface Picture {
+  sources: Record<string, Source[]>;
+  fallback: string;
+}
