e679a4be43cc125cfd710110295e3aeb147ec026
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-07-06 08:32:58 -0700

    fix sample project build

--- examples/vite-simple/vite.config.js
+++ examples/vite-simple/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [imagetools()]
 })
