dfb21cadb61ad3b4f3f6b4ec975dfcc9510d6200
Michael Henson <liegeandlief@users.noreply.github.com>
2023-09-10 09:57:29 +0100

    fix!: generate image IDs from relative filepath

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -1,13 +1,16 @@
 import { createHash } from 'node:crypto'
 import sharp from 'sharp'
 import { ImageConfig } from './types.js'
+import path from 'path'
 
 export function loadImage(path: string) {
   return sharp(path)
 }
 
 export function generateImageID(url: URL, config: ImageConfig) {
-  const baseURL = url.host ? new URL(url.origin + url.pathname) : new URL(url.protocol + url.pathname)
+  const baseURL = url.host
+    ? new URL(url.origin + url.pathname)
+    : new URL(url.protocol + path.relative(process.cwd(), url.pathname))
 
   return createHash('sha1').update(baseURL.href).update(JSON.stringify(config)).digest('hex')
 }
