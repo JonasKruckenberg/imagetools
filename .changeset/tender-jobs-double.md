5cbdbe8998c272d360b196bfaf117faf2a3d0974
Michael <liegeandlief@users.noreply.github.com>
2023-10-07 00:15:05 +0100

    chore: generate image IDs from relative filepath (#605)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

---
+++ .changeset/tender-jobs-double.md
@@ -0,0 +1,5 @@
+---
+'imagetools-core': patch
+---
+
+chore: image IDs are now generated from relative filepaths instead of absolute ones
--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -1,4 +1,5 @@
 import { createHash } from 'node:crypto'
+import path from 'node:path'
 import sharp from 'sharp'
 import { ImageConfig } from './types.js'
 
@@ -7,7 +8,11 @@ export function loadImage(path: string) {
 }
 
 export function generateImageID(url: URL, config: ImageConfig) {
-  const baseURL = url.host ? new URL(url.origin + url.pathname) : new URL(url.protocol + url.pathname)
+  // this isn't a valid URL, but just a string used for an identifier
+  // use a relative path in the local case so that it's consistent across machines
+  const baseURL = url.host
+    ? new URL(url.origin + url.pathname)
+    : new URL(url.protocol + path.relative(process.cwd(), url.pathname))
 
   return createHash('sha1').update(baseURL.href).update(JSON.stringify(config)).digest('hex')
 }
