e8dad5288e55fba81990a6b94ce20c7bb3724f48
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-10-06 16:08:30 -0700

    Update packages/core/src/util.ts

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -11,7 +11,6 @@ export function generateImageID(url: URL, config: ImageConfig) {
   // this isn't a valid URL, but just a string used for an identifier
   const baseURL = url.host
     ? new URL(url.origin + url.pathname)
-    // use a relative path here so that the path is the same locally and on CI
     : new URL(url.protocol + path.relative(process.cwd(), url.pathname))
 
   return createHash('sha1').update(baseURL.href).update(JSON.stringify(config)).digest('hex')
