c7a0bef625ab4225869cd8ab536d926f157f3882
Birk Skyum <birk.skyum@pm.me>
2023-12-02 02:23:39 +0100

    undo change to main.test.ts

--- packages/vite/src/__tests__/main.test.ts
+++ packages/vite/src/__tests__/main.test.ts
@@ -645,7 +645,7 @@ describe('vite-imagetools', () => {
     const { window } = new JSDOM(``, { runScripts: 'outside-only' })
     window.eval(files[0].code)
 
-    expect(window.__IMAGE__).toBe('/assets/with-metadata-Gx2fw5lT.png 600w')
+    expect(window.__IMAGE__).toBe('/assets/with-metadata-_x-Qsbou.png 600w')
   })
 
   test('async output format', async () => {
