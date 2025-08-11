d39cc2cfdb2f5553a7a15f3db45ff5db89110889
Don Denton <don@happycollision.com>
2025-08-11 15:00:07 -0400

    Add `autoOrient` from Sharp 0.34 (#795)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

---
+++ .changeset/wicked-sheep-crash.md
@@ -0,0 +1,6 @@
+---
+'imagetools-core': major
+'vite-imagetools': major
+---
+
+breatking: add `autoOrient` directive from Sharp 0.34.0, defaulting to automatically apply it to every image. An opt-out is available via `noAutoOrient`.
---
+++ packages/core/src/transforms/__tests__/autoOrient.spec.ts
@@ -0,0 +1,96 @@
+import { autoOrient } from '../autoOrient'
+import { resize } from '../resize'
+import { applyTransforms } from '../../index'
+import { consoleLogger } from '../../lib/logger'
+import { TransformFactoryContext } from '../../types'
+import { join } from 'node:path'
+import { toMatchFile } from 'jest-file-snapshot'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
+import sharp, { Sharp } from 'sharp'
+import { describe, beforeAll, beforeEach, test, expect, vi } from 'vitest'
+
+expect.extend({ toMatchFile, toMatchImageSnapshot })
+
+describe('autoOrient', () => {
+  let dirCtx: TransformFactoryContext
+  beforeAll(() => {
+    dirCtx = {
+      // @ts-expect-error vi.fn works fine
+      useParam: vi.fn,
+      manualSearchParams: new URLSearchParams(),
+      logger: consoleLogger
+    }
+  })
+
+  test('keyword "noAutoOrient"', () => {
+    const res = autoOrient({ noAutoOrient: 'true' }, dirCtx)
+
+    expect(res).toBeUndefined()
+  })
+
+  test('missing', () => {
+    const res = autoOrient({}, dirCtx)
+
+    expect(res).toBeInstanceOf(Function)
+  })
+
+  describe('arguments', () => {
+    test('invalid', () => {
+      // @ts-expect-error invalid args
+      const throwingFn = () => autoOrient({ noAutoOrient: 'invalid' }, dirCtx)
+
+      expect(throwingFn).toThrow()
+    })
+
+    test('empty', () => {
+      const res = autoOrient({ noAutoOrient: '' }, dirCtx)
+
+      expect(res).toBeUndefined()
+    })
+
+    test('true', () => {
+      const res = autoOrient({ noAutoOrient: 'true' }, dirCtx)
+
+      expect(res).toBeUndefined()
+    })
+
+    test('false', () => {
+      const res = autoOrient({ noAutoOrient: 'false' }, dirCtx)
+
+      expect(res).toBeInstanceOf(Function)
+    })
+  })
+
+  describe('transform', () => {
+    let img: Sharp
+    beforeEach(() => {
+      img = sharp(join(__dirname, '../../__tests__/__fixtures__/Landscape_5.jpg'))
+    })
+
+    test('autoOrient (no opt-out)', async () => {
+      const metadataBefore = await img.metadata()
+      expect(metadataBefore).toHaveProperty('orientation', 5)
+
+      const { image } = await applyTransforms([autoOrient({}, dirCtx)!], img)
+
+      const result = await image.toBuffer()
+      const metadata = await sharp(result).metadata()
+
+      expect(result).toMatchFile(undefined, { fileExtension: '.jpg' })
+      expect(metadata).not.toHaveProperty('orientation')
+    })
+
+    test('autoOrient (no opt-out) with resize', async () => {
+      const metadataBefore = await img.metadata()
+      expect(metadataBefore).toHaveProperty('orientation', 5)
+
+      const { image } = await applyTransforms([autoOrient({}, dirCtx)!, resize({ w: '150' }, dirCtx)!], img)
+
+      const result = await image.toBuffer()
+      const metadata = await sharp(result).metadata()
+
+      expect(result).toMatchFile(undefined, { fileExtension: '.jpg' })
+      expect(metadata).not.toHaveProperty('orientation')
+    })
+  })
+})
